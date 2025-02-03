017be976f9b1e02259041f8fc6bf36566722cf80
Alex Hagström <alex.hagstrom@student.kyh.se>
2025-02-03 22:44:17 +0100

    added method to decrypt strings using ceasar cipher

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/encrypt", (string input) => Encrypt(input, key));
 
+app.MapGet("/decrypt", (string input) => Decrypt(input, key));
+
 app.Run();
 
 string Encrypt(string input, int key)
@@ -27,4 +29,24 @@ string Encrypt(string input, int key)
         }
     }
     return encryptText;
+}
+
+string Decrypt(string input, int key)
+{
+    string decryptText = "";
+
+    foreach (char c in input)
+    {
+        if (char.IsLetter(c))
+        {
+            char lowerC = char.ToLower(c);
+            char keyed = (char)(((lowerC - key - 'a' + 26) % 26) + 'a');
+            decryptText += keyed;
+        }
+        else
+        {
+            decryptText += c;
+        }
+    }
+    return decryptText;
 }
